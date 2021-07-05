0b8ddad1333e2fcb2bdddef1b63dffc942f29d65
liunengkai <saya@saya.ac.cn>
2021-07-05 23:57:24 +0800

    修改设备网关

---
+++ src/main/java/ac/cn/saya/mqtt/middle/entity/DataFlowEntity.java
@@ -0,0 +1,13 @@
+package ac.cn.saya.mqtt.middle.entity;
+
+/**
+ * @Title: DataFlowEntity
+ * @ProjectName mqtt-middle
+ * @Description: TODO
+ * @Author liunengkai
+ * @Date: 7/5/21 23:56
+ * @Description:
+ */
+
+public class DataFlowEntity {
+}
